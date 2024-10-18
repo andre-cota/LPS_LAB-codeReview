a9ba613c492767a3d615fcb0e87e2f7cd5fed346
Vinicius Rezende <viniciusarantes133@gmail.com>
2024-10-18 07:43:11 -0300

    feat: Adicionar arquivos e configurações iniciais do projeto
    Adiciona arquivos e configurações iniciais do projeto, incluindo o arquivo .gitattributes, compose.yaml, ApiApplicationTests.java, ApiApplication.java, .gitignore, additional-spring-configuration-metadata.json, application-dev.properties, application.properties, application-test.properties, application-prod.properties, maven-wrapper.properties e pom.xml.

---
+++ api/src/test/java/com/lps/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lps.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
